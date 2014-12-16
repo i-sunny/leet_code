cff0317c8bc71ffb43ae23b1335431a84f5f14a4
i-sunny <i_sunn@163.com>
2014-12-16 17:54:01 +0800

    Changes to be committed: 	modified:   10 Valid Parentheses.cpp

--- 10 Valid Parentheses.cpp
+++ 10 Valid Parentheses.cpp
@@ -11,38 +11,6 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-<<<<<<< HEAD
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    ListNode *removeNthFromEnd(ListNode *head, int n) {
-        ListNode *pa = head, pb = pa, pa_pre;
-        int dis = 0;
-        while(pb != NULL){
-            if(dis++ < n)
-                pb = pb->next;
-            else{
-                pa_pre = pa;
-                pa = pa->next;
-                pb = pb->next;
-            }
-        }
-        if(pa == head)
-            head = head->next;
-        else
-            pa_pre->next = pa->next;
-        delete pa;
-        return head;
-    }
-};
-=======
 char cmatch(char c)
 {
     switch(c){
@@ -80,5 +48,3 @@ int main(void)
     cout <<  p << endl;
     return 0;
 }
-
->>>>>>> origin/master
