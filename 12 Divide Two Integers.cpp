75d57afbe6feee44160159e26d2fe67d1ccbe32a
sunny <i_sunn@163.com>
2014-12-17 13:40:34 +0800

    Changes to be committed: 	modified:   12 Divide Two Integers.cpp

--- 12 Divide Two Integers.cpp
+++ 12 Divide Two Integers.cpp
@@ -12,13 +12,14 @@ using std::cin;
 using std::endl;
 
 /*
-* p=q*(1+2+4+...+2^(exp-1))+new_q
+* p=q*(1+2+4+...+2^(exp-1))+new_p
 */
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
-        int exp2 = 0, ret;
-        int p = dividend, q = divisor;
+    int divide(long long dividend, long long divisor) {
+        int exp2 = 0;
+	long long ret;
+        long long p = abs(dividend), q = abs(divisor);
         if(p < q)     //递归出口
             return 0;
 
@@ -27,14 +28,17 @@ public:
             q = q + q;
             exp2++;
         }
-        ret = divide(p, divisor);    //figure out new_q
+        ret = divide(p, abs(divisor));    //figure out new_p
         ret += (0x1 << exp2) - 1;
-        return ret;       
+	if(dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0)
+	    ret = -ret;
+	if(ret >= (unsigned int)(0x1<<31)) --ret;
+	return ret;
     }
 };
 
-void main()
+int main()
 {
     Solution test;
-    cout << test.divide(17,4) << endl;
+    cout << test.divide(1, -2147483648) << endl;
 }
